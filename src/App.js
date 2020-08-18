516943992a285dc8bd21d014348b306693da573b
irene-gomez <irene@irenegf.com>
2020-08-18 18:08:16 +0200

    Add principal structure intl

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
 import { FormattedMessage } from 'react-intl';
+import { Context } from './components/Wrapper';
 
 function App() {
+	const context = useContext(Context);
+	const { locale, selectLang } = context;
+
 	return (
 		<div className="App">
+			<select value={locale} onChange={selectLang}>
+				<option value="en-GB">EN</option>
+				<option value="es-ES">ES</option>
+			</select>
+
 			<ul>
 				<li>
 					<FormattedMessage id="nav.home" defaultMessage="Home" />
--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { IntlProvider } from 'react-intl';
 import App from './App';
-import Spanish from './languages/es-ES.json';
-import English from './languages/en-GB.json';
-
-const locale = navigator.language;
-
-let lang;
-if (locale === 'es-ES') {
-	lang = Spanish;
-} else {
-	lang = English;
-}
+import Wrapper from './components/Wrapper';
 
 ReactDOM.render(
-	<IntlProvider locale={locale} messages={lang}>
+	<Wrapper>
 		<App />
-	</IntlProvider>,
+	</Wrapper>,
 	document.getElementById('root')
 );

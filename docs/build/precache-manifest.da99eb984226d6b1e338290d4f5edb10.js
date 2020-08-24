cf08342a0c358a203ecaf703e1fd9f9d16832a04
irene-gomez <irene@irenegf.com>
2020-08-24 07:07:15 +0000

    Update docs folder

---
+++ docs/build/precache-manifest.da99eb984226d6b1e338290d4f5edb10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbc5e4a6919b8ca96cc82b31234e0c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b5541e45d7861e7f571",
+    "url": "./static/css/main.23e520fb.chunk.css"
+  },
+  {
+    "revision": "3b69c3b407eff5fdefd1",
+    "url": "./static/js/2.aedc4e95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.aedc4e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5541e45d7861e7f571",
+    "url": "./static/js/main.bb1ba9ff.chunk.js"
+  },
+  {
+    "revision": "f6047bd19c7217570e95",
+    "url": "./static/js/runtime-main.c13d98c9.js"
+  }
+]);

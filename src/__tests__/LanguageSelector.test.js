8965f923c2aa484419e5e060da0fc4d91955550a
irene-gomez <irene@irenegf.com>
2020-09-02 21:10:11 +0200

    Fix LanguageSelector test

--- src/__tests__/LanguageSelector.test.js
+++ src/__tests__/LanguageSelector.test.js
@@ -1,28 +1,43 @@
 /* eslint-disable no-undef */
 import React from 'react';
+import { BrowserRouter as Router, browserHistory } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
 import { render, getByTestId } from '@testing-library/react';
 import Wrapper from '../components/Wrapper';
 import LanguageSelector from '../components/Menu';
 
-// eslint-disable-next-line no-undef
 describe('<LanguageSelector />', () => {
 	test('it should exist the component', () => {
-		const { container } = render(<LanguageSelector />, { wrapper: Wrapper });
+		const { container } = render(
+			<Router history={browserHistory}>
+				<LanguageSelector />
+			</Router>,
+			{ wrapper: Wrapper }
+		);
 		const select = getByTestId(container, 'selectLanguage');
 
 		expect(select).toBeTruthy();
 	});
 
 	test('it should contain the text "EN"', () => {
-		const { container } = render(<LanguageSelector />, { wrapper: Wrapper });
+		const { container } = render(
+			<Router history={browserHistory}>
+				<LanguageSelector />
+			</Router>,
+			{ wrapper: Wrapper }
+		);
 		const select = getByTestId(container, 'selectLanguage');
 
 		expect(select).toHaveTextContent('EN');
 	});
 
 	test('it should contain the text "ES"', () => {
-		const { container } = render(<LanguageSelector />, { wrapper: Wrapper });
+		const { container } = render(
+			<Router history={browserHistory}>
+				<LanguageSelector />
+			</Router>,
+			{ wrapper: Wrapper }
+		);
 		const select = getByTestId(container, 'selectLanguage');
 
 		expect(select).toHaveTextContent('ES');
